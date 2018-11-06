8132d1559b2f45105c1fb4f89e3c13d9541d6926
Rüzgar Burak Topal <burak@hayalmahsulleri.com.tr>
2018-11-06 14:57:56 +0300

    added managers to model

--- django_tools/models.py
+++ django_tools/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+from django_tools.managers import SoftDeletionManager
+
 
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_("Yaratılma Tarihi"))
     updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=_("Güncelleme Tarihi"))
     deleted_at = models.DateTimeField(editable=False, verbose_name=_("Silinme Tarihi"), blank=True, null=True)
+
+    objects = SoftDeletionManager()
+    all_objects = SoftDeletionManager(alive_only=False)
 
     class Meta:
         abstract = True
