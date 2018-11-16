b4e29fa90cb83bb4d579f02dd29d5355525ade89
Rüzgar Burak Topal <burak@hayalmahsulleri.com.tr>
2018-11-16 10:35:58 +0300

    added statusable model, queryset and manager

--- django_tools/managers.py
+++ django_tools/managers.py
@@ -2,6 +2,41 @@ from django.db import models
 from django.utils import timezone
 
 
+class StatusableQuerySet(models.QuerySet):
+    def delete(self):
+        return super(StatusableQuerySet, self).update(deleted_at=timezone.now())
+
+    def hard_delete(self):
+        return super(StatusableQuerySet, self).delete()
+
+    def awake(self):
+        return self.filter(status=self.model.ACTIVE)
+
+    def sleeping(self):
+        return self.exclude(status=self.model.PASSIVE)
+
+    def active(self):
+        return super(StatusableQuerySet, self).update(status=self.model.ACTIVE)
+
+    def passive(self):
+        return super(StatusableQuerySet, self).update(status=self.model.PASSIVE)
+
+
+class StatusableManager(models.Manager):
+    _queryset_class = StatusableQuerySet
+
+    def __init__(self, *args, **kwargs):
+        self.awake_only = kwargs.pop('awake_only', True)
+        super(StatusableManager, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        qs = self._queryset_class(self.model, using=self.db, hints=self._hints)
+
+        if self.awake_only:
+            return qs.filter(status=self.model.ACTIVE)
+        return qs
+
+
 class SoftDeletionQuerySet(models.QuerySet):
     def delete(self):
         return super(SoftDeletionQuerySet, self).update(deleted_at=timezone.now())
--- django_tools/models.py
+++ django_tools/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
-from django_tools.managers import SoftDeletionManager
+from django_tools.managers import SoftDeletionManager, StatusableManager
 
 
 class TimeStampedModel(models.Model):
@@ -23,3 +23,30 @@ class TimeStampedModel(models.Model):
 
     def hard_delete(self):
         super(TimeStampedModel, self).delete()
+
+
+class StatusAwareModel(models.Model):
+    ACTIVE = 'active'
+    PASSIVE = 'passive'
+    STATUSES = ()
+
+    _STATUSES = (
+        (ACTIVE, _('Aktif')),
+        (PASSIVE, _('Pasif'))
+    ) + STATUSES
+
+    status = models.CharField(max_length=30, verbose_name=_('Durum'), blank=True, default=ACTIVE, choices=_STATUSES)
+
+    objects = StatusableManager()
+    all_objects = StatusableManager(awake_only=False)
+
+    class Meta:
+        abstract = True
+
+    def active(self):
+        self.status = self.ACTIVE
+        self.save()
+
+    def passive(self):
+        self.status = self.PASSIVE
+        self.save()
